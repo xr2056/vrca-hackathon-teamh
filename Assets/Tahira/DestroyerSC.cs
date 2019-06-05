7ca1d30ea3df9bb00199060935edb7378aa553d0
atsushitahira <a.tahira3@icloud.com>
2019-06-05 12:32:24 +0900

    スタートボタンとベルトコンベアのスクリプトを作成
    tagを追加しています。
    積み木用のオブジェクトには「tsumiki」
    プレイヤーの手には「hand」

---
+++ Assets/Tahira/DestroyerSC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerSC : MonoBehaviour {
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "tsumiki")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
