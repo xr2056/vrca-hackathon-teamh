041c5f8bec9654f71001bde3275f431d0518c737
kuro <kuro.xr5@gmail.com>
2019-06-07 15:56:22 +0900

    コア部分の実装（タイマー、スコア） アセットの整理

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Score : MonoBehaviour
+{
+
+    public GameObject counttest;
+    public LayerMask m_LayerMask;
+    float[] scores = new float[20];
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        MyCollisions();
+    }
+
+    private void MyCollisions()
+    {
+        Collider[] colliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            float score = colliders[i].transform.position.y;
+            scores[i] = score;
+
+        }
+        MaxScore();
+    }
+
+    void MaxScore()
+    {
+        float results = scores.Max();
+        counttest.GetComponent<TextMesh>().text = results.ToString("F2");
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    float countTime;
+    public float Limit = 0;
+    float TimeLimit;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += Time.deltaTime;
+        TimeLimit = Limit - countTime;
+        if (TimeLimit > 0.1)
+        {
+            GetComponent<TextMesh>().text = TimeLimit.ToString("F2");
+
+        }
+        else
+        {
+            GetComponent<TextMesh>().text = ("Finish");
+            Time.timeScale = 0;
+        }
+    }
+}
