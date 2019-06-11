20ec69a8d16fefe79ab37f0290c33cc96ebc4524
KurokiMasami <cherry8823@icloud.com>
2019-06-11 22:20:00 +0900

    積み木をベルトコンベアから床に設置した際、回転アニメーションを外すため親子関係解除。

---
+++ Assets/Scripts/ParentNull.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentNull : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "floor")
+            transform.parent = null;
+    }
+
+}
