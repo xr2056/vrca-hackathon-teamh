760884da6cd30ff1a30dcb6ae831985c0b4af7cc
atsushitahira <a.tahira3@icloud.com>
2019-06-11 15:39:26 +0900

    スタートボタンのスクリプトを作成しました

--- Assets/Scripts/StartButtonSC.cs
+++ Assets/Scripts/StartButtonSC.cs
@@ -1,29 +1,16 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class StartButtonSC : MonoBehaviour
-{
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    public GameObject cubePrefab;
-    public GameObject spherePrefab;
-    public GameObject capsulePrefab;     public GameObject spawner;
+public class StartButtonSC : MonoBehaviour {
 
-    void OnCollisionEnter(Collision col)
+void OnTriggerEnter(Collider col)
     {
-           
-
-
         if (col.gameObject.tag == "hand")
         {
-            //オブジェクトの初期化
-            GameObject[] oldobjs = GameObject.FindGameObjectsWithTag("tsumiki");
-            foreach (GameObject oldobj in oldobjs)
-            {
-                Destroy(oldobj);
-            }             Instantiate(cubePrefab, spawner.transform.position, Quaternion.identity);             ////複数の場所に一気にオブジェクトを出す
-            //for (int i = -2; i < 3; i += 2)
-            //{
-            //    int objrd = Random.Range(1, 11);
-            //    GameObject obj = objrd > 8 ? capsulePrefab : (objrd > 5 ? spherePrefab : cubePrefab);
-            //    Instantiate(obj, new Vector3(i, 2, 3), Quaternion.identity);
-            //}
+            //タイマーとタイマーのスクリプトを名前指定で拾ってきて、スクリプトを有効にする
+            GameObject.Find("Timer").GetComponent<Timer>().enabled = true;
+            this.gameObject.SetActive(false);
         }
     }
 }
