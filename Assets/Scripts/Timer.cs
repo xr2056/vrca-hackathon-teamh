575dca7c2ae10a5e2ed7c9ca8b494556c40f1439
KurokiMasami <cherry8823@icloud.com>
2019-06-13 20:07:14 +0900

    UI表示、スコアボード作成、ライティングをしました。

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,10 +9,15 @@ public class Timer : MonoBehaviour
     public float Limit = 0;
     float TimeLimit;
 
+    //ui用に追加
+    public GameObject ui;
+    public GameObject restartbutton;
+
     // Use this for initialization
     void Start()
     {
         Time.timeScale = 1;
+
     }
 
     // Update is called once per frame
@@ -30,6 +35,21 @@ public class Timer : MonoBehaviour
             FindObjectOfType<Score>().Save();
             GetComponent<TextMesh>().text = ("Finish");
             Time.timeScale = 0;
+
+            //ui用に追加
+            StartCoroutine("UI");
         }
+    }
+
+
+    //ui用に追加
+    private IEnumerator UI()
+    {
+        yield return new WaitForSecondsRealtime(5.0f);
+        ui.SetActive(true);
+        yield return new WaitForSecondsRealtime(5.0f);
+        Time.timeScale = 1;
+        restartbutton.SetActive(true);
+
     }
 }
