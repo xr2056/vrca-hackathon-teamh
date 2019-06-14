ddb52ffa82e14d8457f4e27ee54b9ef286da73d0
KurokiMasami <cherry8823@icloud.com>
2019-06-14 19:12:30 +0900

    BGM・SE適用、ライティング修正しました。

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -5,14 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour {
 
+    public AudioClip pushsound;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
         //タイマーとタイマーのスクリプトを名前指定で拾ってきて、スクリプトを有効にする
-        GameObject.Find("Timer").GetComponent<Timer>().enabled = true;
-        this.gameObject.SetActive(false);
-        GameObject.Find("StartButton").gameObject.SetActive(false);
+        //GameObject.Find("Timer").GetComponent<Timer>().enabled = true;
+        //this.gameObject.SetActive(false);
+        //GameObject.Find("StartButton").gameObject.SetActive(false);
+
+        StartCoroutine("restartprocess");
 
     }
+
+
+    private IEnumerator restartprocess()
+    {
+        audioSource.PlayOneShot(pushsound, 1.0F);
+        yield return new WaitForSecondsRealtime(0.3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
 
 }
